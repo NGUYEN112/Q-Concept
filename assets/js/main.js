480860e63bcf213aec161c9fa5cd5a773d5b7f67
Nguyen <phannguyenhoainguyen@gmail.com>
2023-04-15 23:13:42 +0700

    create fixedbar & contact page

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,29 @@ $(window).ready(function () {
 });
 $(window).on("load", function () {
   
+  const logoSrcWhite = "./assets/images/logo.png";
+  const logoSrcBlack = "./assets/images/logo-revert.png"
   setInterval(() => {
     $(".maxims__border").toggleClass("collapse");
   }, 4800);
   $(".hasAni").addClass("animation");
-    if(document.getElementById("video1") && document.getElementById("video2") && document.getElementById("video3") && document.getElementById("video4")){
-      document.getElementById("video1").addEventListener("ended", function(){ playVideo("video2","video1"); });
-      document.getElementById("video2").addEventListener("ended", function(){ playVideo("video3","video2"); });
-      document.getElementById("video3").addEventListener("ended", function(){ playVideo("video4","video3"); });
-      document.getElementById("video4").addEventListener("ended", function(){ playVideo("video1","video4"); });
+
+  $(".navBar__hambuger").click(function(){
+    $(".navBar__hambuger").toggleClass("active");
+    $(".navBar__fixedBar").toggleClass("active");
+    if($(".navBar__hambuger").hasClass("active")) {
+      $("#logoCommon").attr("src",logoSrcWhite);
+    }else {
+      $("#logoCommon").attr("src",logoSrcBlack);
     }
+  })
+  // auto play video bg 
+  if(document.getElementById("video1") && document.getElementById("video2") && document.getElementById("video3") && document.getElementById("video4")){
+    document.getElementById("video1").addEventListener("ended", function(){ playVideo("video2","video1"); });
+    document.getElementById("video2").addEventListener("ended", function(){ playVideo("video3","video2"); });
+    document.getElementById("video3").addEventListener("ended", function(){ playVideo("video4","video3"); });
+    document.getElementById("video4").addEventListener("ended", function(){ playVideo("video1","video4"); });
+  }
     
   function playVideo(videoID,currentVideoID){
     let videoElement = document.getElementById(videoID);
@@ -21,14 +34,13 @@ $(window).on("load", function () {
     videoElement.classList.remove("hide")
     videoElement.play();
   }
+
+//  lazy load image 
   const observerObtions = {
     root: null,
     rootMargin: "0px",
     threshold: 0.2
   }
-
-
-
   const lazyLoadingCallBack = (elements, observer) => {
     elements.forEach((element,index) => {
       if(element.isIntersecting) {
@@ -36,19 +48,9 @@ $(window).on("load", function () {
         const dataSrc = img.dataset.src
         img.setAttribute('src',dataSrc)
         img.classList.add("fadeIn")
-        // if(index === 0 || index % 2 ==0) {
-        //   element.target.classList.add('fadeInRight')
-        // }else {
-        //   element.target.classList.add('fadeInLeft')
-        // }
         element.target.classList.add('fadeIn')
       }else {
         element.target.classList.remove('fadeIn')
-        // if(index === 0 || index % 2 ==0 ) {
-        //   element.target.classList.remove('fadeInRight')
-        // }else {
-        //   element.target.classList.remove('fadeInLeft')
-        // }
       }
     })
   }
@@ -57,19 +59,3 @@ $(window).on("load", function () {
 
   lazyElement.forEach(ele => observer.observe(ele))
 });
-
-// let windowHeight = $(window).outerHeight();
-
-// if (windowWidth > 768) {
-//   $(window).scroll(function () {
-//     let windowTop = $(window).scrollTop();
-//     var indexEle = 1;
-//     var breakPoint = 0;
-//     var elementHeight =  $(".scale#item" + indexEle).height();
-//     if((windowTop +  elementHeight - breakPoint) > maxEleHeight) {
-//       indexEle += 1;
-//       breakPoint = windowTop - max
-//     }
-//     console.log($(".scale#item-"+indexEle).height(),);
-//   });
-// }
